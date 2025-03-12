c071fdfb8981827ac5cb416317f5781ea7bdbf6e
June <128846125+junearranz@users.noreply.github.com>
2025-03-12 11:04:14 +0100

    Subiendo el ejercicio 2

---
+++ ejercicio_2.py
@@ -0,0 +1,10 @@
+# Crear una cadena que contenga la palabra "Hola". 
+# Usando la palabra clave en el método de búsqueda o el índice, busque y seleccione "Hola" en su cadena. 
+# Y usando la función de reemplazo, reemplace "Hola" en su cadena con "adiós".
+
+string = 'Ella se acercó y me dijo hola'
+print(string[25:])
+string = string.replace("hola","adios")
+print(string)
+
+
